40eb2bd48f952bd1a25cac003def69c9c6ab85d6
Lee JongHwan <jong479@gmail.com>
2021-09-07 16:15:29 +0900

    2장 5절 View resolvers 완료
    jsp저장 폴더 이름을 views로 변경

--- src/main/java/com/ezen/webstore/config/DispatcherServletInitializer.java
+++ src/main/java/com/ezen/webstore/config/DispatcherServletInitializer.java
@@ -19,7 +19,7 @@ public class DispatcherServletInitializer
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/" };
+        return new String[] { "/app/*" };
     }
 }
 
