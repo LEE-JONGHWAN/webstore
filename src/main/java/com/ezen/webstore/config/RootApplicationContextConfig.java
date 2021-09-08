57dc6c525e7dde9d9a229212102485dab0176000
Lee, JongHwan <jong479@gmail.com>
2021-09-08 12:37:12 +0900

    unnecessary @componentScan removed from root'config

--- src/main/java/com/ezen/webstore/config/RootApplicationContextConfig.java
+++ src/main/java/com/ezen/webstore/config/RootApplicationContextConfig.java
@@ -3,13 +3,11 @@ package com.ezen.webstore.config;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
-@ComponentScan("com.ezen.webstore")
 public class RootApplicationContextConfig {
 
 	// MariaDB 사용 때:
