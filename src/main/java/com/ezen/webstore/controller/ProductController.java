758f20aa422d147610d50a45bc5d0dcfcf67c38e
Lee, JongHwan <jong479@gmail.com>
2021-09-17 12:04:40 +0900

    ResponseStatus - request handler exception resolve
    gracefully using annotation.

--- src/main/java/com/ezen/webstore/controller/ProductController.java
+++ src/main/java/com/ezen/webstore/controller/ProductController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ezen.webstore.domain.Product;
+import com.ezen.webstore.exception.NoProductsFoundUnderCategoryException;
 import com.ezen.webstore.service.ProductService;
 
 
@@ -145,8 +146,11 @@ public class ProductController {
 	@RequestMapping("/products/{category}")
 	public String getProductsByCategory(Model model, 
 			@PathVariable("category") String category) {
-		model.addAttribute("products", 
-				productService.getProductsByCategory(category));
+		var products = productService.getProductsByCategory(category); 
+		if (products == null || products.isEmpty()) {
+			throw new NoProductsFoundUnderCategoryException();
+		}
+		model.addAttribute("products", products);
 
 		return "products";
 	}
